a067822abc1d895d5ff80a7a0d89aeca9621709f
Michał Słapek <28485371+mslapek@users.noreply.github.com>
2022-06-19 13:08:17 +0200

    Replace training_maximum_duration in letstune.tune with rounds parameter

--- examples/keras/advanced_mnist_epochtrainer.py
+++ examples/keras/advanced_mnist_epochtrainer.py
@@ -102,9 +102,12 @@ trainer = MNISTTrainer()
 
 tuning = letstune.tune(
     trainer,
-    16,
+    8,
     results_dir=Path.home() / "ltexamples/keras/advanced_mnist_epochtrainer",
-    training_maximum_duration=timedelta(seconds=30),
+    rounds=[
+        timedelta(seconds=2),
+        timedelta(seconds=4),
+    ],
 )
 print(f" DONE: {tuning}")
 
